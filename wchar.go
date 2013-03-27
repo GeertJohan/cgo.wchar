8f3c4f2ce29119f92c44fa7bc2e81a456d97cd49
GeertJohan <gjr19912@gmail.com>
2013-03-27 01:25:50 +0100

    Work in progress :D

--- wchar.go
+++ wchar.go
@@ -31,13 +31,14 @@ func FromGoString(input string) (output []C.wchar_t, err error) {
 	}
 	defer C.iconv_close(iconv)
 
-	// calculate bufferSize in bytes
-	bufferSize := len([]byte(input))
+	// calculate bufferSizes in bytes
+	bufferSizeIn := len([]byte(input)) // count exact amount of bytes
+	bufferSizeOut := len(input) * 4    // wide char seems to be 4 bytes for every single- or multi-byte character. Not very sure though.
 
-	// bufferSizes for C
-	bytesLeftIn := bufferSize
+	// bufferSizes for C (copies, because iconv will touch them)
+	bytesLeftIn := bufferSizeIn
 	bytesLeftInCSize := C.size_t(bytesLeftIn)
-	bytesLeftOut := bufferSize * 4 //FIXME: wide chars seems to be 4 bytes for 1 char. Not very sure though.
+	bytesLeftOut := bufferSizeOut
 	bytesLeftOutCSize := C.size_t(bytesLeftOut)
 
 	// input for C
@@ -45,7 +46,7 @@ func FromGoString(input string) (output []C.wchar_t, err error) {
 	defer C.free(unsafe.Pointer(inputCString))
 
 	// create output buffer
-	outputChars := make([]int8, bytesLeftOut)
+	outputChars := make([]int8, bufferSizeOut)
 
 	// output for C
 	outputCString := (*C.char)(&outputChars[0])
@@ -54,23 +55,74 @@ func FromGoString(input string) (output []C.wchar_t, err error) {
 	spew.Dump(x)
 	spew.Dump(errno)
 
-	output = make([]C.wchar_t, 0, bufferSize)
-	for len(outputChars) > bytesLeftOut/4 {
+	output = make([]C.wchar_t, 0, len(input))
+	for len(outputChars) >= 4 {
 		// create 4 position byte slice
 		b := make([]byte, 4)
 		b[0] = byte(outputChars[0])
 		b[1] = byte(outputChars[1])
 		b[2] = byte(outputChars[2])
 		b[3] = byte(outputChars[3])
+		uchar := binary.LittleEndian.Uint32(b)
+		if uchar == 0 { // find null terminator (doing this right?)
+			break
+		}
 		// Combine 4 position byte slice into uint32, and append uint32 to outputUint32
-		output = append(output, C.wchar_t(binary.LittleEndian.Uint32(b)))
+		output = append(output, C.wchar_t(uchar))
 		// reslice the outputChars
 		outputChars = outputChars[4:]
 	}
 
-	a := int32(12)
-	b := (C.wchar_t)(a)
-	spew.Dump(b)
-
 	return output, nil
+}
+
+func ToGoString(input *C.wchar_t) (output string, err error) {
+	// open iconv
+	iconv, errno := C.iconv_open(strUtf8, strWchar)
+	if iconv == nil || errno != nil {
+		return "", fmt.Errorf("Could not create iconv instance: %s", errno)
+	}
+	defer C.iconv_close(iconv)
+
+	inputAsChars := make([]C.char, 0)
+	for {
+		nextWchar := *input
+		b := make([]byte, 4)
+		binary.LittleEndian.PutUint32(b, uint32(nextWchar))
+		spew.Dump(b)
+		if b[0] == 0 { // find null terminator (doing this right?)
+			break
+		}
+		//++ do something with b
+		//++ split b into seperate bytes, make those int8's.. make those int8's C.char again. Add the C.char to inputAsChars
+
+		break // remove when pointer arithmic is working and actual loop can be done
+	}
+
+	// input for C
+	inputAsCharsFirst := &inputAsChars[0]
+
+	// calculate buffer size for input
+	bufferSizeIn := len(inputAsChars)
+	bufferLeftIn := bufferSizeIn
+	bytesLeftInCSize := C.size_t(bufferLeftIn)
+
+	// calculate buffer size for output
+	bufferSizeOut := len(inputAsChars)
+	bytesLeftOut := bufferSizeOut
+	bytesLeftOutCSize := C.size_t(bytesLeftOut)
+
+	// create output buffer
+	outputChars := make([]int8, bufferSizeOut)
+
+	// output for C
+	outputCString := (*C.char)(&outputChars[0])
+
+	x, errno := C.iconv(iconv, &inputAsCharsFirst, &bytesLeftInCSize, &outputCString, &bytesLeftOutCSize)
+	spew.Dump(x)
+	spew.Dump(errno)
+
+	//++ do processing on outputChars
+
+	return "todo", nil
 }
